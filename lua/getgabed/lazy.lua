7a9c795eafd050c5d30e818e67094373c6bae9c6
Adissuu <gabrieldubois.eng@gmail.com>
2024-04-19 13:57:28 -0400

    removed notifications when adding plugin

--- lua/getgabed/lazy.lua
+++ lua/getgabed/lazy.lua
@@ -11,4 +11,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("getgabed.plugins", {})
+require("lazy").setup("getgabed.plugins", {
+  change_detection = {
+    notify = false,
+  },
+
+})
